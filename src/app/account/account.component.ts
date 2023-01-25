62eacc9511f60b647949dae1d3da9efb0d39b0bd
Jose-Alisson <84239112+Jose-Alisson@users.noreply.github.com>
2023-01-25 13:12:25 -0300

    pizzaria

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,21 +1,30 @@
+import { SignInService } from './../shared/services/signIn/sign-in.service';
 import {
   FacebookLoginProvider,
   GoogleLoginProvider,
   SocialAuthService,
 } from '@abacritt/angularx-social-login';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss'],
 })
-export class AccountComponent implements OnInit {
-  constructor(private authService: SocialAuthService) {}
+export class AccountComponent implements OnInit, AfterViewInit {
+
+  constructor(private authService: SocialAuthService, private signInService: SignInService) {}
+
+  loginWithGoogle() {
+    document .querySelector('asl-google-signin-button') ?.querySelector('div')  ?.lastElementChild?.querySelector('div') ?.click();
+  }
+
+  ngAfterViewInit(): void {
+  }
 
   ngOnInit(): void {
     this.authService.authState.subscribe((user) => {
-      console.log(user);
+
     });
   }
 
@@ -23,7 +32,7 @@ export class AccountComponent implements OnInit {
     this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
   }
 
-  sair(){
-    this.authService.signOut()
+  sair() {
+    this.authService.signOut();
   }
 }
