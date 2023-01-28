a1da8a238e42fa60cd901e1d19ef48a4f181a8f1
Jose-Alisson <84239112+Jose-Alisson@users.noreply.github.com>
2023-01-28 17:25:22 -0300

    pizzaria

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -2,14 +2,14 @@ import { SocialUser } from '@abacritt/angularx-social-login';
 import { Router } from '@angular/router';
 import { SignInService } from 'src/app/shared/services/sign-in.service';
 import { Usuario } from './../../model/usuario.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
 
   userFromPs!: Usuario;
 
@@ -21,6 +21,21 @@ export class AboutComponent implements OnInit {
       this.socialUser = signIn.socialUser;
       this.userFromPs = signIn.userFromPs;
     }
+  }
+  ngAfterViewInit(): void {
+    let fade = document.getElementById('fade')
+    let modal = document.querySelector('#modal-add-addrss')
+
+    document.querySelector('.close')?.addEventListener('click', () => {
+      modal?.classList.add('desatived')
+      fade?.classList.add('desatived')
+    })
+
+    let btnAdd = document.getElementById('btn-add')
+    btnAdd?.addEventListener('click', ()  => {
+      modal?.classList.remove('desatived')
+      fade?.classList.remove('desatived')
+    })
   }
 
   ngOnInit(): void {}
